25cdbcf688d5c44a50f28141dea4d228c62fc72c
Albin Axtelius <albin@axtelius.se>
2022-09-03 23:27:45 +0200

    Added dark mode support

--- components/AxteliusTitles.tsx
+++ components/AxteliusTitles.tsx
@@ -1,22 +1,22 @@
-import React from "react";
+import React from 'react'
 
 const AxteliusTitles: React.FC = (props) => {
-  return (
-    <div
-      data-testid="axtelius-titles"
-      className="text-2xl md:text-6xl text-center font-mono mt-8"
-    >
-      <h1>
-        <span className="font-extrabold">www</span>.axtelius.se
-      </h1>
-      <h1>
-        www.<span className="font-extrabold">axtelius</span>.se
-      </h1>
-      <h1>
-        www.axtelius.<span className="font-extrabold">se</span>
-      </h1>
-    </div>
-  );
-};
+	return (
+		<div
+			data-testid="axtelius-titles"
+			className="mt-8 text-center font-mono text-2xl text-black dark:text-white md:text-6xl"
+		>
+			<h1>
+				<span className="font-extrabold">www</span>.axtelius.se
+			</h1>
+			<h1>
+				www.<span className="font-extrabold">axtelius</span>.se
+			</h1>
+			<h1>
+				www.axtelius.<span className="font-extrabold">se</span>
+			</h1>
+		</div>
+	)
+}
 
-export default AxteliusTitles;
+export default AxteliusTitles
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,6 @@
-import {
-	mdiGit,
-	mdiLanguageHtml5,
-	mdiLanguageJavascript,
-	mdiLanguageTypescript,
-	mdiTailwind
-} from '@mdi/js'
-import { GetStaticProps, GetStaticPropsContext } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import { useEffect } from 'react'
+import { GetStaticProps } from 'next'
 import AxteliusTitles from '../components/AxteliusTitles'
 import Contacts from '../components/Contacts'
-import TechnologyTile, { TintColor } from '../components/TechnologyTile'
 import { getContactInfo } from '../services/FirebaseService'
 import { ContactInfo } from '../types'
 
@@ -21,37 +10,12 @@ type Props = {
 
 export default function Home({ contactInfo }: Props) {
 	return (
-		<div className="container h-screen py-6 mx-auto">
-			<div className="flex flex-col items-center justify-between h-full">
-				<AxteliusTitles />
-				<div className="flex items-center justify-center">
-					<TechnologyTile
-						tooltip="Tailwind CSS"
-						path={mdiTailwind}
-						tintColor={TintColor.green}
-					/>
-					<TechnologyTile
-						tooltip="TypeScript"
-						path={mdiLanguageTypescript}
-						tintColor={TintColor.blue}
-					/>
-					<TechnologyTile
-						tooltip="HTML"
-						path={mdiLanguageHtml5}
-						tintColor={TintColor.orange}
-					/>
-					<TechnologyTile
-						tooltip="Git"
-						path={mdiGit}
-						tintColor={TintColor.red}
-					/>
-					<TechnologyTile
-						tooltip="JavaScript"
-						path={mdiLanguageJavascript}
-						tintColor={TintColor.yellow}
-					/>
+		<div className="min-h-screen w-screen bg-white dark:bg-black">
+			<div className="mx-auto h-screen py-6">
+				<div className="flex h-full flex-col items-center justify-between">
+					<AxteliusTitles />
+					<Contacts contactInfo={contactInfo} />
 				</div>
-				<Contacts contactInfo={contactInfo} />
 			</div>
 		</div>
 	)
